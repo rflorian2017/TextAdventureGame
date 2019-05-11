6246a81ec51a2c88ad83c4d60ecb81b11f2fc067
rflorian2017 <roby_florian@yahoo.com>
2019-05-11 11:33:51 +0300

    First commit , draft implementation

--- src/game/model/Player.java
+++ src/game/model/Player.java
@@ -1,4 +1,18 @@
 package game.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    /**
+     * items that the player can collect
+     */
+    private List<Artifact> artifacts;
+
+    /**
+     * Create a player that has a list of collected items
+     */
+    public Player() {
+        artifacts = new ArrayList<>();
+    }
 }
