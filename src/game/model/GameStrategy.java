66a57e25cc9eff256cbbd597f9f89c8ef9ad2d71
rflorian2017 <roby_florian@yahoo.com>
2019-05-19 12:46:50 +0300

    Refactor game into gameboard - correct remove collectible

--- src/game/model/GameStrategy.java
+++ src/game/model/GameStrategy.java
@@ -1,9 +1,42 @@
 package game.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class GameStrategy {
+    private static HashMap<String, List<String>> commandsMap; // e. g. move ; [east, west, north, south, up, down, right, left]
+
+    static {
+        commandsMap.put("move", new ArrayList<>());
+        List<String> possibleValuesForMove = new ArrayList<>();
+        possibleValuesForMove.add("east");
+        possibleValuesForMove.add("west");
+        possibleValuesForMove.add("south");
+        possibleValuesForMove.add("north");
+        possibleValuesForMove.add("right");
+        possibleValuesForMove.add("left");
+        possibleValuesForMove.add("up");
+        possibleValuesForMove.add("down");
+        possibleValuesForMove.add("e");
+        possibleValuesForMove.add("w");
+        possibleValuesForMove.add("n");
+        possibleValuesForMove.add("s");
+        possibleValuesForMove.add("r");
+        possibleValuesForMove.add("l");
+        possibleValuesForMove.add("u");
+        possibleValuesForMove.add("d");
+        commandsMap.get("move").addAll(possibleValuesForMove);
+        commandsMap.put("go", possibleValuesForMove);
+    }
+
     public static void processCommand(String command, Game game, Player player) {
+
         String[] commands = command.split(" ");
-        //e.g move 2 5 -> commands[0] = move, commands[1] = 2, commands[2] = 5
+        if(commandsMap.containsKey(commands[0])) {
+            //game.movePlayer()
+        }
+        //e.g move 2 5 -> commandsMap[0] = move, commandsMap[1] = 2, commandsMap[2] = 5
         if(commands[0].equals("move")) {
             game.movePlayer(player ,
                     Integer.parseInt(commands[1]),
